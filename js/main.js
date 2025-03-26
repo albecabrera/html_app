b1d1400b277d404e898ebbae27e8db257a75b313
albecabrera <albe.cabrera@yahoo.de>
2025-03-26 15:24:38 +0100

    cambio3

---
+++ js/main.js
@@ -0,0 +1,21 @@
+let saludo = 'Hola Mundo';
+console.log(saludo);
+
+
+// con charArt podemos acceder a un caracter en una posición específica de un string.
+let texto1 = "Esto es un mensaje de línea única\n\n\n Y esta es otra".charAt(1);
+let texto2 = "Esto es un mensaje de línea única\n\n\n Y esta es otra"[2];
+console.log(texto1);
+console.log(texto2); 
+console.log(texto2.charAt(4)); 
+
+
+ // Datos numéricos 
+let num1 = 10;
+let num2 = 20;
+let resultado = num1 * num2;
+let resultado2 = num2 / num1;
+console.log(resultado2);
+console.log(resultado); // 200
+console.log(num1 + num2); // 30
+console.log(num1 - num2); // -10
